25ff369adc7e0921898149de2a6f45fe5d1d003e
carlosyague@gmail.com <carlosyague@gmail.com@0e6bcf1b-9ac7-caac-59c1-400bc5207f8c>
2011-09-06 21:40:46 +0000

    Versión de cliente de REST [OK] #4

--- trunk/spring3-resttemplate/src/test/java/es/sopragroup/core/RestClient.java
+++ trunk/spring3-resttemplate/src/test/java/es/sopragroup/core/RestClient.java
@@ -19,44 +19,30 @@ package es.sopragroup.core;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import es.sopragroup.core.dao.rest.CustomerClient;
-import es.sopragroup.core.dao.rest.FlickrClient;
 import es.sopragroup.core.dao.rest.UrlTxtClient;
-
 
 
 public class RestClient {
 
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.out.println("Usage: java " + RestClient.class.getName() + " [Flickr API key] [search term]");
+        if (args.length < 2) {
+            System.out.println("Usage: java " + RestClient.class.getName() + " [web-service] [url]");
             System.exit(-1);
         }
-        String apiKey = args[0];
-        String searchTerm = args[1];
-        String wsclient = args[2];
+        final String wsclient = args[0];
+        final String url = args[1];
         
-        if ("flickr".equals(wsclient)) {
-        	ApplicationContext applicationContext =
-                    new ClassPathXmlApplicationContext("applicationContext.xml", RestClient.class);
-            FlickrClient client = applicationContext.getBean("flickrClient", FlickrClient.class);
-            client.doIt(apiKey, searchTerm);
-        } else if ("customer".equals(wsclient)) {
-        	ApplicationContext applicationContext =
-                    new ClassPathXmlApplicationContext("applicationContext.xml", RestClient.class);
-            CustomerClient client = applicationContext.getBean("customerClient", CustomerClient.class);
-            client.doIt();
-        } else if ("shortenURL".equals(wsclient)) {
+        if ("shortenURL".equals(wsclient)) {
         	ApplicationContext applicationContext =
                     new ClassPathXmlApplicationContext("applicationContext.xml", RestClient.class);
         	UrlTxtClient client = applicationContext.getBean("urlTxtClient", UrlTxtClient.class);
-            final String shortUrl = client.shortenUrl("http://www.yahoo.es/pop3");
+            final String shortUrl = client.shortenUrl(url);
             System.out.println("shortUrl="+shortUrl);
         } else if ("expandURL".equals(wsclient)) {
         	ApplicationContext applicationContext =
                     new ClassPathXmlApplicationContext("applicationContext.xml", RestClient.class);
         	UrlTxtClient client = applicationContext.getBean("urlTxtClient", UrlTxtClient.class);
-        	final String longUrl = client.expandUrl("c81e728d9d4c2f636f067f89cc14862c");
+        	final String longUrl = client.expandUrl(url);
             System.out.println("longUrl="+longUrl);
         }
         

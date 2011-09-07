340d3e7b78e921151854697e9e8853c137ef1fba
carlosyague@gmail.com <carlosyague@gmail.com@0e6bcf1b-9ac7-caac-59c1-400bc5207f8c>
2011-09-07 16:41:32 +0000

    Versión de cliente de REST [TERMINADO] #3

---
+++ trunk/spring3-resttemplate/src/main/java/es/sopragroup/urlshortener/core/dao/IUrlShortenerDAO.java
@@ -0,0 +1,36 @@
+
+package es.sopragroup.urlshortener.core.dao;
+
+public interface IUrlShortenerDAO {
+
+	/**
+	 * 
+	 * @param shortUrl
+	 * @return
+	 */
+	String expandUrl(String shortUrl);
+
+	/**
+	 * 
+	 * @param shortUrl
+	 * @param server
+	 * @return
+	 */
+	String expandUrl(String shortUrl, String server);
+
+	/**
+	 * 
+	 * @param longUrl
+	 * @return
+	 */
+	String shortenUrl(String longUrl);
+
+	/**
+	 * 
+	 * @param longUrl
+	 * @param server
+	 * @return
+	 */
+	String shortenUrl(String longUrl, String server);
+
+}
--- trunk/spring3-resttemplate/src/main/java/es/sopragroup/urlshortener/core/util/UrlUtil.java
+++ trunk/spring3-resttemplate/src/main/java/es/sopragroup/urlshortener/core/util/UrlUtil.java
@@ -1,4 +1,4 @@
-package es.sopragroup.core.util;
+package es.sopragroup.urlshortener.core.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;

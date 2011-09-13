eb9f2476ecccb33cf74339f7a508b7f9e6f3c795
carlosyague@gmail.com <carlosyague@gmail.com@0e6bcf1b-9ac7-caac-59c1-400bc5207f8c>
2011-09-13 11:10:14 +0000

    Web - config. properties #2

--- url-shortener-web/src/main/java/org/urlshortener/web/utils/UrlShortenerUtil.java
+++ url-shortener-web/src/main/java/org/urlshortener/web/utils/UrlShortenerUtil.java
@@ -2,13 +2,42 @@ package org.urlshortener.web.utils;
 
 import org.urlshortener.core.service.IUrlShortenerWsManager;
 
-public class UrlShortenerUtil {
-	
-	public static final String CLOUD_HTTP_SERVER = "http://url-shortener-ws.cloudfoundry.com";
+/**
+ * Clase de utilidades de URL-Shortening 
+ * 
+ * @author cyague
+ *
+ */
+public final class UrlShortenerUtil {
 
+	private static final String DEFAULT_CLOUD_HTTP_SERVER="http://url-shortener-ws.cloudfoundry.com";
+	
+	/**
+	 * Obtiene la instancia de Spring del manager que ofrece servicios de
+	 * url-shortening mediante conexión a un WS
+	 * 
+	 * @return
+	 */
 	public static IUrlShortenerWsManager getManager() {
-		return (IUrlShortenerWsManager) SpringUtil.getBean("urlShortenerWsManager",
-				IUrlShortenerWsManager.class,
+		return (IUrlShortenerWsManager) SpringUtil.getBean(
+				"urlShortenerWsManager", IUrlShortenerWsManager.class,
 				"classpath*:/spring/applicationContext-service-ws.xml");
 	}
+
+	/**
+	 * Obtiene el valor de la propiedad con la ruta del servidor web en el que
+	 * se encuentran los WS se url-shortening<br>
+	 * . Esta propiedad se especifica en el fichero 'application.properties'
+	 * 
+	 * @return
+	 */
+	public static String getCloudHttpServer() {
+		String result = ConfigUtil.getConfigProperty("CLOUD_HTTP_SERVER");
+		
+		if (result == null) {
+			result = DEFAULT_CLOUD_HTTP_SERVER;
+		}
+		return result;
+	}
+
 }
